9a3e4f2a7c6b697a3219e198408e0466c26dfb52
Alejandro Muñiz Siques <amunizsi@emeal.nttdata.com>
2024-10-23 11:02:18 -0300

    funciones principales de extraccion creadas, iniciacion de condicionales en caso de pop ups o mensajes de error

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
-$x('//span[contains(@class,"btn-text") and contains(text(),"SALDOS Y MOV.TARJETAS CRÉDITO")]').map(x => x.textContent)
+import  extract_xml
+from dotenv import  dotenv_values
+config = dotenv_values(".env")
+
+
+try:
+
+   driver = extract_xml.web_driver_initialzation(bank_url =config["BANK_URL"] )
+
+except Exception as e:
+
+    print("the inizalization of the driver failed")
+    print(e)
+
+try:
+
+    extract_xml.login_user( driver = driver,
+                            user_name = config["USER_NAME"],
+                            password = config["PASSWORD"] )
+except Exception as e:
+
+    print("the login procces had failed")
+    print(e)
+
+try:
+    extract_xml.download_excel(driver = driver)
+
+except Exception as e:
+    print("the extraction proccess had filed")
+    print(e)
+
+
+
--- extract_xml.py
+++ extract_xml.py
@@ -2,4 +2,58 @@ import selenium
 import os
 import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from dotenv import  dotenv_values
+
+# envarioment variables being load from a .venv
+config = dotenv_values(".env")
+
+# start of the module based in funtions
+
+def web_driver_initialzation(**kwargs): # funtion that start the webdriver and is directed to the initial page of the bank
+
+    bank_url = kwargs.get("bank_url")
+
+    driver = webdriver.Chrome()
+
+    driver.get(bank_url)
+
+    time.sleep(3)
+
+    return driver
+
+
+def login_user(**kwargs):
+     
+     driver = kwargs.get("driver")
+     user_name = kwargs.get("user_name")
+     password = kwargs.get("password")
+
+     if driver.find_element(By.XPATH, '//p[contains(text,"Por favor intente ingresar nuevamente a nuestro portal.")]'):
+          driver.find_element(By.XPATH,'//button[contains(@class,"success btn pull-right")]')
+
+
+
+
+
+
+
+
+     driver.find_element(By.XPATH, '//a[contains(@id,"ppp_header-link-banco_en_linea")]').click()
+     time.sleep(1)
+     driver.find_element(By.XPATH, '//input[contains(@class,"form-control ng-pristine ng-invalid ng-invalid-rut ng-invalid-required ng-valid-maxlength ng-touched") and contains(@placeholder, "Rut Usuario")]').send_keys(user_name)
+     time.sleep(1)
+     driver.find_element(By.XPATH, '//input[contains(@name,"userpassword") and contains(@placeholder, "Clave")]').send_keys(password)
+     time.sleep(1)
+     driver.find_element(By.XPATH, '//button[contains(@class,"btn success btn-block") and contains(@id, "idIngresar")]').click()
+     time.sleep(3)
+
+def download_excel(**kwargs):
+     
+     driver = kwargs.get("driver")
+     driver.find_element(By.XPATH,'//span[contains(@class, "btn-text") and contains(text(), "SALDOS Y MOV.TARJETAS CRÉDITO")]').click()
+     time.sleep(2) 
+     driver.find_element(By.XPATH,'//span[contains(@class, "btn-text") and contains(text(), "Descargar")]').click()
+
